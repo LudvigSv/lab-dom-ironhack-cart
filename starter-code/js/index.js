96a153ed4ac7b6bb1edeb3184c93680d7a7ead1f
Ludvig Svensson <ludvigsvensson@Ludvigs-MacBook-Air.local>
2018-01-26 16:40:06 -0500

    not finished

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,51 @@
-function deleteItem(e){
+function deleteItem(e) {}
 
+function getPriceByProduct(itemNode) {}
+
+function updatePriceByProduct(productPrice, index) {}
+
+// remove child
+function getTotalPrice() {}
+
+function createQuantityInput() {}
+
+function createDeleteButton() {}
+
+function createQuantityNode() {}
+
+function createItemNode(dataType, itemData) {}
+
+function createNewItemRow(itemName, itemUnitPrice) {}
+
+function createNewItem() {}
+
+function addEventToQuantity() {
+  var input = document.getElementsByClassName("quantityInputs");
 }
 
-function getPriceByProduct(itemNode){
+window.onload = function() {
+  var calculatePriceButton = document.getElementsByClassName(
+    "calc-prices-button"
+  );
 
-}
+  button.onclick = function() {
+    console.log(calculatePriceButton() + "Calculating Price");
+  };
 
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var createItemButton = document.getElementById("new-item-create");
+  var deleteButtons = document.getElementsByClassName("btn-delete");
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
+};
+
+var button = document.getElementsByTagName("button")[0];
+
+button.onclick = function() {
+  var input = document.getElementsByTagName("input")[0];
+  console.log(input.value);
 };
